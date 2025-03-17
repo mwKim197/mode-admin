3c49290ccd8b8f535af0667607e01c71d2804196
minwoo kim <mwkim197@gmail.com>
2025-03-17 11:06:51 +0900

    로그인페이지 수정

--- src/ts/api.ts
+++ src/ts/api.ts
@@ -15,7 +15,7 @@ export async function fetchWithAuth(endpoint: string, options: RequestInit = {})
 
         if (!isLoginPage) {
             console.log("❌ 토큰이 없습니다. 로그인 페이지로 이동합니다.");
-            window.location.href = "index.html"; // ✅ 로그인 페이지로 이동
+            window.location.href = "../../index.html"; // ✅ 로그인 페이지로 이동
         }
         return null;
     }
