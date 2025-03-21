49e330504f5df9e591859613c77f12801a387200
minwoo kim <mwkim197@gmail.com>
2025-03-21 14:28:36 +0900

    공지사항등록 테스트

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from "vite";
+import { resolve } from "path";
 
 export default defineConfig({
+    base: "./", // ✅ 경로를 상대 경로로 설정 (필수!)
     css: {
         postcss: "./postcss.config.js",
     },
@@ -10,6 +12,12 @@ export default defineConfig({
     build: {
         commonjsOptions: {
             transformMixedEsModules: true, // ✅ CommonJS 패키지를 ESM으로 변환
+        },
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'index.html'),                   // 기본 entry
+                notice: resolve(__dirname, 'public/html/notice.html'),   // 추가된 entry
+            },
         },
     },
 });
