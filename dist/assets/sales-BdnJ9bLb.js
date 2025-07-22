90252cc5ae3b596e497c9d289a05df26fad087e4
mwkim <mwkim197@gmail.com>
2025-07-22 23:22:50 +0900

    수정사항반영

--- dist/assets/sales-BdnJ9bLb.js
+++ dist/assets/sales-BdnJ9bLb.js
@@ -1,4 +1,4 @@
-import{a as pe}from"./main-BZgyfFOO.js";/*!
+import{a as pe}from"./main-lJcv5wPf.js";/*!
  * html2canvas 1.4.1 <https://html2canvas.hertzen.com>
  * Copyright (c) 2022 Niklas von Hertzen <https://hertzen.com>
  * Released under MIT License
